ba7aa4c46ecbb4fbdc9d7a08039825cd105f8cf7
Ella Tanttu <ellajtanttu@gmail.com>
2021-11-04 18:07:36 -0700

    add ui logic
    Co-authored-by: Jeff Terrell <jeffaterrell@gmail.com>
    co-authored-by: Kate Kiatsiri <keidsiri@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 // Business Logic
 
-function Player(roll, turnScore, totalScore) {
+function Player(roll, turnScore, totalScore, turn) {
   this.roll = roll;
   this.turnScore = turnScore;
   this.totalScore = totalScore;
+  this.turn = turn;
 }
 
 // new game button "Let's play again!" ***needs $("#gameboard").show***
@@ -25,31 +26,25 @@ Player.prototype.rollDice = function (){
     }
   } else {
     this.turnScore = 0;
-    toggle();
+    this.toggle();
+    console.log("toggle triggered in rollDice()")
   }
 }
 
-Player.prototype.rollDice = function (){
-  rollNumber = Math.ceil(Math.random() * 6);
-  console.log("rollNumber " + rollNumber);
-  this.roll = rollNumber;
-  console.log("this.roll is " + this.roll);
-  if ( this.roll > 1 ) {
-    if (this.totalScore + this.turnScore + this.roll >= 100) {
-      return this.winner();
-    } else {
-      return this.turnScore += this.roll;
-    }
+Player.prototype.toggle = function() {
+  if (this.turn === true) {
+    console.log(this.turn + " toggled from true to false")
+    return this.turn = false;
   } else {
-    this.turnScore = 0;
-    toggle();
+    console.log(this.turn + " toggled from false to true")
+    return this.turn = true;
   }
 }
 
 Player.prototype.clickHold = function() {
   this.totalScore += this.turnScore;
-  this.turnscore = 0;
-  toggle();
+  this.turnScore = 0;
+  this.toggle();
 }
 
 Player.prototype.winner = function() {
@@ -57,15 +52,90 @@ Player.prototype.winner = function() {
   $("#winner").show();
 }
 
+function toggle() {
+  $("#player1roll , #player1hold").toggle();
+  $("#player2roll , #player2hold").toggle();
+}
 
 // User Interface Logic:
 
-function toggle () {
-  $("")
-}
 
 $(document).ready(function() {
-  $("#").submit(function(event) {
+  $("#playerForm").submit(function(event) {
+    event.preventDefault();
+    console.log("form submitted");
+    const nameOne = $("input#p1Name").val();
+    const nameTwo = $("input#p2Name").val();
+
+    let playerOne = new Player(0, 0, 0, true);
+    let playerTwo = new Player(0, 0, 0, false);
+    $(".gameboard").show();
+    // $("#player2roll").hide();
+    // $("#player2hold").hide();
+
+    // if (playerOne.turn === true) {
+    //   $("#player1roll").show();
+    //   $("#player1hold").show();
+    //   $("#player2roll").hide();
+    //   $("#player2hold").hide();
+    // } else {
+    //   $("#player1roll").hide();
+    //   $("#player1hold").hide();
+    //   $("#player2roll").show();
+    //   $("#player2hold").show();
+    //   return playerTwo.toggle();
+    // }
+
+    // if (playerTwo.turn === true) {
+    //   $("#player2roll").show();
+    //   $("#player2hold").show();
+    //   $("#player1roll").hide();
+    //   $("#player1hold").hide();
+    // } else {
+    //   $("#player2roll").hide();
+    //   $("#player2hold").hide();
+    //   $("#player1roll").show();
+    //   $("#player1hold").show();
+    //   return playerOne.toggle();
+    // }
+
+    $("#player1roll").click(function() {
+      playerOne.rollDice();
+      console.log("player one round score is " + playerOne.turnScore);
+      $("#currentroll").html(playerOne.roll);
+      $("#player1roundscore").html(playerOne.turnScore);
+    });
+
+    $("#player1hold").click(function() {
+      playerOne.clickHold();
+      console.log("player one total score is " + playerOne.totalScore);
+      $("#player1totalscore").html(playerOne.totalScore);
+      playerOne.toggle();
+      console.log("p1 toggled" + playerOne.turn)
+      playerTwo.toggle();
+      console.log("p2 toggled" + playerTwo.turn)
+    });
+
+    $("#player2roll").click(function() {
+      playerTwo.rollDice();
+      console.log("player two round score is " + playerTwo.turnScore);
+      $("#currentroll").html(playerTwo.roll);
+      $("#player2roundscore").html(playerTwo.turnScore);
+    });
+
+    $("#player2hold").click(function() {
+      playerTwo.clickHold();
+      console.log("player two total score is " + playerTwo.totalScore);
+      $("#player2totalscore").html(playerTwo.totalScore);
+      console.log("p1 toggled" + playerOne.turn)
+      playerTwo.toggle();
+      console.log("p2 toggled" + playerTwo.turn)
+    });
+
+
 
   });
 });
+
+
+
